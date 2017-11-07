508dd96c08f4aa4c537dcd7888fd22489e8bcbf2
AJ Pryor <apryor6@gmail.com>
2017-11-07 15:43:58 -0500

    basic GENFIRE module implemented

--- tomviz/python/Recon_GENFIRE.py
+++ tomviz/python/Recon_GENFIRE.py
@@ -1,16 +1,43 @@
 import genfire as gf
 import tomviz.operators
+import numpy as np
 
 class ReconGENFIREOperator(tomviz.operators.CancelableOperator):
-    def transform_scalars(self, Niter=50, OversamplingRatio=3, GriddingMethod="FFT", ResolutionExSupp=1,
-     InterpolationCutoffDistance=0.5):
-        print("GENFIRE transform_scalars invoked")
-        # Get Tilt angles
-        tilt_angles = utils.get_tilt_angles(dataset)
+    def transform_scalars(self, dataset, **kwargs):
+
+        # Get tilt angles
+        from tomviz import utils
+        tiltAngles = utils.get_tilt_angles(dataset)
+
+        # Convert to Euler representation assuming single-tilt axis
+        eulerAngles = np.zeros((len(tiltAngles), 3))
+        eulerAngles[:, 1] = tiltAngles
 
         tiltSeries = utils.get_array(dataset)
         if tiltSeries is None:
             raise RuntimeError("No scalars found!")
-        Nslice = tiltSeries.shape[0]
-        print("TiltAngles = {}".format(tilt_angles))
-        print("Nslice = {}".format(Nslice))
+
+        # Convert enumerated type to that used by GENFIRE
+        gridMap = {0:"FFT", 1:"DFT"}
+        kwargs['griddingMethod'] = gridMap[kwargs['griddingMethod']]
+
+        # Assemble rest of parameters
+        pars = dict(
+            projections = tiltSeries,
+            eulerAngles = eulerAngles,
+            resultsFilename = "recon.mrc",
+            **kwargs
+            )
+
+        # Run the reconstruction
+        GF = gf.reconstruct.GenfireReconstructor(**pars)
+        results = GF.reconstruct()
+
+        from vtk import vtkImageData
+        recon_dataset = vtkImageData()
+        recon_dataset.CopyStructure(dataset)
+        utils.set_array(recon_dataset, results['reconstruction'])
+        utils.mark_as_volume(recon_dataset)
+        
+        return dict(reconstruction=recon_dataset)
+        # return dict(reconstruction=results['reconstruction'])
